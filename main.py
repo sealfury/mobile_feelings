e21f417b836a1d16dc5be6dcdbdbe8b12aefd981
sealfury <willspecker@gmail.com>
2021-02-23 12:35:04 +0100

    creates add_user method on sign up button click creates json file for user data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file("ux_design.kv")
 
@@ -13,8 +15,22 @@ class LoginScreen(Screen):
 class RootWidget(ScreenManager):
     pass
 
+
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[uname] = {
+            "username": uname,
+            "password": pword,
+            "created": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        }
+
+        with open("users.json", "w") as file:
+            json.dump(users, file)
+
+
 class MainApp(App):
     def build(self):
         return RootWidget()
