8301d8bb5b8b04e87cd61f44e4a95e877c3f0ce0
sealfury <willspecker@gmail.com>
2021-02-23 14:26:18 +0100

    adds styling to quote generation screen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 import json, glob
 from datetime import datetime
 from pathlib import Path
@@ -69,6 +72,8 @@ class LoginScreenSuccess(Screen):
         else:
             self.ids.quote.text = "Try another feeling"
 
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 class MainApp(App):
     def build(self):
