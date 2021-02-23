5160d78d3074bf5d66b8e168e8cac33856818d1d
sealfury <willspecker@gmail.com>
2021-02-23 12:04:19 +0100

    imports kv widgets into main py app

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from kivy.app import App 
+from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file('ux_design.kv')
+Builder.load_file("ux_design.kv")
+
+
+class LoginScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+if __name__ == "__main__":
+    MainApp().run()
